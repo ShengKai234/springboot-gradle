fafbc671447bdb2a55ec798b560226e7fcb58934
ShengKai234 <bboykai0504@gmail.com>
2020-08-30 22:03:57 +0800

    update

---
+++ src/test/java/io/kai/springbootgradle/SpringbootGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.kai.springbootgradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
