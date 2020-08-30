67409168675359b3e7268b867b0e6f07971e4f96
ShengKai234 <bboykai0504@gmail.com>
2020-08-31 02:00:34 +0800

    update gradle html

--- src/main/java/io/kai/springbootgradle/controller/HelloController.java
+++ src/main/java/io/kai/springbootgradle/controller/HelloController.java
@@ -3,15 +3,35 @@ package io.kai.springbootgradle.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
+@RequestMapping("/")
 public class HelloController {
-	@GetMapping
-    public Map hello() {
-        Map map = new HashMap();
-        map.put("say", "hello");
-        return map;
+	
+	@RequestMapping("/index1")
+    public ModelAndView hello() {
+		System.err.println("hello");
+		ModelAndView model = new ModelAndView("index1");
+        model.addObject("msg", "Hello world");
+        return model;
     }
+	
+	 @RequestMapping("/index2")
+     String home() {
+		 System.err.println("index2");
+       return "index/index2";
+     }
+	 
+	 @GetMapping("/index3")
+     String index() {
+		 System.err.println("index3");
+       return "index/index3";
+     }
 }
